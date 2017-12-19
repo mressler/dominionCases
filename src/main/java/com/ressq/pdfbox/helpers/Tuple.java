23fa4fb082e75365f51642d7f9c11e4ad24889b2
Michael Ressler <mressler@diamondkinetics.com>
2017-12-19 08:30:43 -0500

    Multi-Type tuple. Because why not?

--- src/main/java/com/ressq/pdfbox/helpers/Tuple.java
+++ src/main/java/com/ressq/pdfbox/helpers/Tuple.java
@@ -1,8 +1,14 @@
 package com.ressq.pdfbox.helpers;
 
-public class Tuple<T> {
+public class Tuple<T, U> {
 
 	public T x;
-	public T y;
+	public U y;
+	
+	public Tuple() {}
+	public Tuple(T x, U y) {
+		this.x = x;
+		this.y = y;
+	}
 	
 }
